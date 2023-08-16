9cad5474888c2ff608c9a479b449ef56522b8c00
Tanvir <omorfarukqqq@gmail.com>
2023-08-17 02:32:40 +0600

    arithmetic and asigment operator

---
+++ arithmetic.php
@@ -0,0 +1,26 @@
+<?php 
+//arithmetic operators
+$num1 =10;
+$num2 = 20;
+$add = $num1+$num2;//addition
+$sub = $num1+$num2;//substraction
+$div = $num1/$num2;//divition
+$m = $num1/$num2;//modulus
+$multiple = $num1*$num2;//multiple
+$power = $num1**$num2;//Exponetation
+echo $sub;
+echo $add;
+echo $div;
+echo $m;
+echo $mmultiple;
+echo $power;
+//asignment operator
+$num1+=$num2;
+echo $num1;
+$num1-=$num2;
+echo $num1;
+$num1*=$num2;
+echo $num1;
+$num1%=$num2;
+echo $num1;
+?>
