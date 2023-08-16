9f62932ea3e2740d06688fd2b1122231301516f7
Tanvir <omorfarukqqq@gmail.com>
2023-08-17 02:49:17 +0600

    logical operator

---
+++ logical_operators.php
@@ -0,0 +1,15 @@
+<?php
+$var1 = true;
+$var2 = false;
+
+
+
+var_dump( $var1 and $var2);
+var_dump( $var1 or $var2);
+var_dump( $var1 || $var2);
+var_dump( $var1 xor $var2);
+var_dump( !$var1);
+var_dump( !$var2);
+
+
+?>
