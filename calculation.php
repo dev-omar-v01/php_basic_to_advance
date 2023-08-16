af286b21b58a00af9f64af736318780f808b73ea
Tanvir <omorfarukqqq@gmail.com>
2023-08-17 01:09:36 +0600

    calculation php

---
+++ calculation.php
@@ -0,0 +1,19 @@
+<?php 
+$value1=12345;
+$value2=12345;
+
+$plus=$value1+$value2;
+$minus=$value1-$value2;
+$multply=$value1*$value2;
+$divide=$value1/$value2;
+
+
+echo $plus;
+echo "\n";
+echo $minus;
+echo "\n";
+echo $multply;
+echo "\n";
+echo $divide;
+
+?>
