c23c9c65ef79fdd193e5d38acb53f08b04122187
Tanvir <omorfarukqqq@gmail.com>
2023-08-17 02:54:34 +0600

    increment or decrement

---
+++ increment_or_decrement.php
@@ -0,0 +1,18 @@
+<?php
+//incremnet
+$num1 =10;
+$result = ++$num1;
+echo $result;
+$num2 =20;
+$result2 = $num2++;
+echo $result2;
+//decrement
+$num3 =10;
+$resul3 = --$num1;
+echo $result3;
+$num4 =20;
+$result2 = $num4--;
+echo $result4;
+
+
+?>
--- logical_operators.php
+++ logical_operators.php
@@ -2,8 +2,6 @@
 $var1 = true;
 $var2 = false;
 
-
-
 var_dump( $var1 and $var2);
 var_dump( $var1 or $var2);
 var_dump( $var1 || $var2);
