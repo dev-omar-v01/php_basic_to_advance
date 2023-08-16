dcdf098359ad9b81dde88f88226c44e76e174486
Tanvir <omorfarukqqq@gmail.com>
2023-08-17 01:46:07 +0600

    constran php

---
+++ constran.php
@@ -0,0 +1,27 @@
+<?php 
+//define()
+//const keyword
+define('NUMBER',50);
+ 
+echo NUMBER;
+
+//case sensitive using false
+define('NUM',50,false);
+echo NUm;
+
+//not case sensitive using true
+//case sensitive using false
+define('NU',50,true);
+echo Nu;
+
+
+const NUM1=50;
+echo NUM1;
+
+
+//constran not follow any variable scoping rules.
+//the can be defined accesse anywhere
+//const -> global
+//variable ->global,local ,static
+
+?>
