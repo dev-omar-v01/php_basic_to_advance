f094cd057ed8c007d940b8a8887bdce081b662b7
Tanvir <omorfarukqqq@gmail.com>
2023-08-17 01:33:22 +0600

    variable php

---
+++ variable.php
@@ -0,0 +1,17 @@
+<?php 
+//String
+$name ='md omor faruk';
+//Number
+$num =13;
+//boolean true or false true allwase 1 return
+$bool =true;
+//array
+$fruits =["apple","banana","mango"];
+
+$num1 =10;
+$num2=10;
+$result=$num1+$num2;
+echo "Our result is :$result";
+
+
+?>
