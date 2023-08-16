7dfc3d356aac3ce6e152b72ad59cd46be52398ed
Tanvir <omorfarukqqq@gmail.com>
2023-08-17 02:01:48 +0600

    comon primitive data type php

---
+++ comon_primitive_data_type.php
@@ -0,0 +1,31 @@
+<?php
+//String
+$name ='md omor faruk';
+var_dump($name);
+$name1 =' ?';
+var_dump("omor"."faruk");
+//integer
+$num = 10;
+var_dump($num);
+//float all so called double
+$num1 = 10.0;
+var_dump($num);
+//boolean 
+$bool = true;
+var_dump($bool);
+
+//
+$fruits =["apple","banana","mango",10,10.0,true,false];
+echo $fruits;
+//object
+class Car{
+
+}
+$car = new Car();
+var_dump($car);
+
+//NULL 
+$null = NULL;
+var_dump($null);
+
+?>
