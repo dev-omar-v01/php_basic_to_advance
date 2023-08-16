90a48395e657dcdad26b79651caf48d8db65dbe6
Tanvir <omorfarukqqq@gmail.com>
2023-08-17 02:41:29 +0600

    relations or comperasion operator

---
+++ relation_or_comparision_operators.php
@@ -0,0 +1,13 @@
+<?php
+$num1 =10;
+$num2= 20;
+var_dump($num1 == $num2);//equal
+var_dump($num1 === $num2);//identical
+var_dump($num1 !== $num2);//not equal
+var_dump($num1 <> $num2);//not equal
+var_dump($num1 < $num2);//less then
+var_dump($num1 <= $num2);//less then or equal to
+var_dump($num1 <=> $num2);//spaceship
+
+
+?>
