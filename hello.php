1c0bd1b3804825c4e265ef2bab90d4b36de8f150
softdevomar <omorfarukqqq@gmail.com>
2023-11-09 04:49:06 -0800

    index.php html code addede

--- hello.php
+++ hello.php
@@ -1,3 +1,5 @@
 <?php 
 echo "hello";
+//my print
+echo  "\nMy name is Omar faruk";
 ?>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    //this is a single line command
+    /*
+      this is a multiple line camment
+    */
+    $result = 5 /*this is a 2 number plus*/ +10;
+    echo "result is $result <br>";
+    
+      ECHO "hello <br>";
+     ?>
+</body>
+</html>
